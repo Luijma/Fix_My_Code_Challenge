d2f65b09bb6a94ff0f8325795c7b9ba2edf95cc4
Luijma <rsluismanuel@gmail.com>
2022-06-03 19:06:42 +0000

    t2

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
 
+
 class User():
-    """ Documentation """
+    """ Create an object named User """
 
     def __init__(self):
-        """ Documentation """
+        """ initialize User object attributes """
         self.__email = None
+
+    @property
+    def email(self):
+        """getter for private instance attribute email"""
+        return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
+        """ setter for User's email """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
+
 if __name__ == "__main__":
 
     u = User()
