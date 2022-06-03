009a7a0a10ca80258671df4ccc94921fc0b6279c
Luijma <rsluismanuel@gmail.com>
2022-06-03 18:59:14 +0000

    t1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -24,6 +24,7 @@ class Square():
         """Returns the string representation of a Square object"""
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
     s = Square(width=12, height=9)
